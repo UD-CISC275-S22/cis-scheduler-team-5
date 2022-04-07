7751a3f4c32e70b3738d185c16bd0b2d5921572d
NabihaS <nazasy@udel.edu>
2022-04-07 15:05:21 -0400

    created welcome message

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,13 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript - Nabiha Syed, Mar
                 Tejedor, Alexis Mainiero
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <div>
+                <h1>Welcome to your degree scheduler!</h1>{" "}
+                <p>
+                    We are here to help you find clarity on your courses path at
+                    UD. You can get started by viewing your courses.
+                </p>
+            </div>
         </div>
     );
 }
