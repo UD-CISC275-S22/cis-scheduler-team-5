fb1d5f067cabdbfc4b0ab9a35ba30ed2186f9fb4
martejedor <martejedor11@gmail.com>
2022-04-26 16:57:37 -0400

    mtejedor fix warnings

--- src/components/ShowSemesters.tsx
+++ src/components/ShowSemesters.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import { Course } from "../interfaces/course";
-import { CourseEditor } from "./Courses";
-export function ShowSemesters({
+//import { Course } from "../interfaces/course";
+//import { CourseEditor } from "./Courses";
+export function ShowSemesters(/*{
     catalog
 }: {
     catalog: Record<string, Record<string, Course>>;
-}): JSX.Element {
+}*/): JSX.Element {
     return (
         <div>
             <Container>
