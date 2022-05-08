ad6ea6d7f30ddeb5179ecc00894473d2f928de36
NabihaS <nazasy@udel.edu>
2022-05-08 19:55:02 -0400

    testing showsemester

---
+++ src/components/ShowSemesters.test.tsx
@@ -0,0 +1,19 @@
+export {};
+/*import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { ShowSemesters } from "./ShowSemesters";
+import userEvent from "@testing-library/user-event";
+
+test("renders the word Course somewhere", () => {
+    render(<ShowSemesters catalog={catalog} />);
+    const linkElement = screen.getByText(/Fall/i);
+    expect(linkElement).toBeInTheDocument();
+});
+
+
+describe("ShowSemesters Component tests", () => {
+    beforeEach(() => {
+        render(<ShowSemesters />);
+    });
+});
+*/
